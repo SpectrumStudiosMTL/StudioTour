ff6a8f3b2dba7d60a9aed52f1b14531dff95dd5c
SpectrumStudiosMTL <cameron@productionsspectrum.com>
2026-06-03 10:46:37 -0400

    Upload

---
+++ data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-photo360",
+      "name": "photo360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.078159488937919,
+        "pitch": 0,
+        "fov": 1.313201992424292
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.1747068490794064,
+          "pitch": -0.8825255523414413,
+          "title": "Lighting",
+          "text": "GVM LED PANELS"
+        },
+        {
+          "yaw": 1.7429963671727453,
+          "pitch": 0.3515024958778614,
+          "title": "c-stands",
+          "text": "3 available with rental"
+        }
+      ]
+    }
+  ],
+  "name": "Spectrum Studios",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
